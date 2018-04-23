010cbf5c983857b64b4b260425e992759a50aad7
feyyaz.akkus <feyyazakkus@gmail.com>
2018-04-23 16:00:18 +0300

    create server for production

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, function () {
+    console.log("Server running on port 8080..");
+});
