0db7adb913cff3932ea6d0897fdaece72e63ce9b
feyyaz.akkus <feyyazakkus@gmail.com>
2018-08-09 20:33:37 +0300

    change api url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
 export const appConfig = {
-    //apiUrl: 'https://customer-management-api.herokuapp.com/api'
-    apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'https://customer-management-api.herokuapp.com/api'
+    //apiUrl: 'http://localhost:3000/api'
 }
